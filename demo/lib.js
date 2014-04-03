c98c7dc01298547e4e5a41e0c0fbfd1b0c21676f
Alex Warth <alexwarth@gmail.com>
2014-04-03 11:32:36 -0700

    semi-colonization :)

--- demo/lib.js
+++ demo/lib.js
@@ -1,10 +1,10 @@
 function makeElement(tagName) {
-  var element = document.createElement(tagName)
+  var element = document.createElement(tagName);
   for (var idx = 1; idx < arguments.length; idx++) {
-    var child = typeof arguments[idx] === 'string' ?  document.createTextNode(arguments[idx]) : arguments[idx]
-    element.appendChild(child)
+    var child = typeof arguments[idx] === 'string' ?  document.createTextNode(arguments[idx]) : arguments[idx];
+    element.appendChild(child);
   }
-  return element
+  return element;
 }
 
 function removeChildren(element) {
@@ -14,12 +14,14 @@ function removeChildren(element) {
 }
 
 function show(divId, what) {
-  if (!(what instanceof Node))
-    what = document.createTextNode('' + what)
-  var div = document.getElementById(divId)
-  while (div.firstChild)
-    div.removeChild(div.firstChild)
-  div.appendChild(what)
+  if (!(what instanceof Node)) {
+    what = document.createTextNode('' + what);
+  }
+  var div = document.getElementById(divId);
+  while (div.firstChild) {
+    div.removeChild(div.firstChild);
+  }
+  div.appendChild(what);
 }
 
 function repeat(s, n) {
