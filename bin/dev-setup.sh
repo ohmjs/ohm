014b9b3287e43607475190bc5859092835df901c
Patrick Dubroy <pdubroy@gmail.com>
2015-03-19 12:47:41 +0100

    Add a dev-setup script that is run on postinstall.
    This script sets up a few things that are useful for developers who
    are working on Ohm.

---
+++ bin/dev-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script sets up a few things helpful for developers who are contributing
+# to this package. It is intended to be run automatically in the postinstall
+# hook, but can also be run manually.
+
+EXEC_NAME=$(basename $0)
+ROOT=$(npm prefix)
+
+# If .gitignore does not exist, we are not in developer mode.
+# See .npmignore for more details.
+if [ ! -f "$ROOT/.gitignore" ]; then exit; fi
+
+GIT_DIR=$(git rev-parse --git-dir 2> /dev/null)
+
+if [ "$GIT_DIR" == "" ]; then
+  echo "$EXEC_NAME: Unable to find .git directory."
+  exit 1
+fi
+
+# Install a merge driver called "ours" that is just an alias to `true`.
+# This is referenced by .gitattributes, to prevent conflicts with files
+# that are generated during the build process.
+git config --add merge.ours.driver true
+
+# Try to install a project-specific pre-commit hook.
+cp -n "$ROOT/bin/pre-commit" "$GIT_DIR/pre-commit"
+if [ $? -ne 0 ]; then
+  echo "$EXEC_NAME: pre-commit script already exists; not overwriting."
+fi
+
+echo "$EXEC_NAME: Developer setup complete."
