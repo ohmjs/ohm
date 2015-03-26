a87df0803b07ffaa8a9d62a6589b5d1af3c77d96
Patrick Dubroy <pdubroy@gmail.com>
2015-03-26 09:57:04 +0100

    Fix problems with dev setup script.
    - pre-commit was being copied to the wrong directory
    - duplicate entries were being added for the merge driver

--- bin/dev-setup.sh
+++ bin/dev-setup.sh
@@ -21,10 +21,10 @@ fi
 # Install a merge driver called "ours" that is just an alias to `true`.
 # This is referenced by .gitattributes, to prevent conflicts with files
 # that are generated during the build process.
-git config --add merge.ours.driver true
+git config --get merge.ours.driver > /dev/null || git config --add merge.ours.driver true
 
 # Try to install a project-specific pre-commit hook.
-cp -n "$ROOT/bin/pre-commit" "$GIT_DIR/pre-commit"
+cp -n "$ROOT/bin/pre-commit" "$GIT_DIR/hooks/pre-commit"
 if [ $? -ne 0 ]; then
   echo "$EXEC_NAME: pre-commit script already exists; not overwriting."
 fi
