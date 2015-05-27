f24a133a53d4fc70b48651719d35d229bf4b7c66
Patrick Dubroy <pdubroy@gmail.com>
2015-05-27 12:54:42 +0200

    deploy-gh-pages: Change username and email.

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -7,8 +7,8 @@ if [ -n "$CI" ]; then
   git remote set-branches --add origin gh-pages &&
   git fetch origin &&
   git branch gh-pages origin/gh-pages &&
-  git config user.name "Travis CI" &&
-  git config user.email "travis@w3ctag.org"
+  git config user.name "bin/deploy-gh-pages.sh" &&
+  git config user.email "ci@invalid"
 fi
 
 git rev-parse --quiet --verify gh-pages > /dev/null || (echo "No gh-pages branch found."; exit 1)
