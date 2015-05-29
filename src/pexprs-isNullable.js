a9bc78badd90e7024faef2620e7ddc5b876cfa67
Alessandro Warth <alexwarth@gmail.com>
2015-05-28 19:04:21 -0700

    added a file that I forgot to add in 7e2dd7c

---
+++ src/pexprs-isNullable.js
@@ -0,0 +1,65 @@
+'use strict';
+
+// --------------------------------------------------------------------
+// Imports
+// --------------------------------------------------------------------
+
+var pexprs = require('./pexprs');
+
+// --------------------------------------------------------------------
+// Operations
+// --------------------------------------------------------------------
+
+// Returns `true` if this parsing expression may accept without consuming any input.
+pexprs.PExpr.prototype.isNullable = function(grammar) {
+  return this._isNullable(grammar, Object.create(null));
+};
+
+pexprs.PExpr.prototype._isNullable = function(grammar, memo) {
+  return false;
+};
+
+pexprs.end._isNullable = function(grammar, memo) {
+  return true;
+};
+
+pexprs.StringPrim.prototype._isNullable = function(grammar, memo) {
+  // This is an over-simplification: it's only correct if the input is a string. If it's an array
+  // or an object, then the empty string parsing expression is not nullable.
+  return this.obj === '';
+};
+
+pexprs.Alt.prototype._isNullable = function(grammar, memo) {
+  return this.terms.length === 0 ||
+      this.terms.some(function(term) { return term._isNullable(grammar, memo); });
+};
+
+pexprs.Seq.prototype._isNullable = function(grammar, memo) {
+  return this.factors.every(function(factor) { return factor._isNullable(grammar, memo); });
+};
+
+pexprs.Many.prototype._isNullable = function(grammar, memo) {
+  return this.minNumMatches === 0;
+};
+
+pexprs.Opt.prototype._isNullable =
+pexprs.Not.prototype._isNullable =
+pexprs.Lookahead.prototype._isNullable = function(grammar, memo) {
+  return true;
+};
+
+pexprs.Str.prototype._isNullable = function(grammar, memo) {
+  // This is also an over-simplification that is only correct when the input is a string.
+  return this.expr._isNullable(grammar, memo);
+};
+
+pexprs.Apply.prototype._isNullable = function(grammar, memo) {
+  var key = this.toMemoKey();
+  if (!Object.prototype.hasOwnProperty.call(memo, key)) {
+    var body = grammar.ruleDict[this.ruleName];
+    var inlined = body.substituteParams(this.params);
+    memo[key] = false;
+    memo[key] = inlined._isNullable(grammar, memo);
+  }
+  return memo[key];
+};
