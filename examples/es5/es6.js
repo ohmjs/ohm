2db1686b6eaa536ab77568e6857e3787e7503d7c
Patrick Dubroy <pdubroy@gmail.com>
2015-06-16 21:32:33 +0200

    Add beginnings of an ES6 grammar.

---
+++ examples/es5/es6.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(ohm, ns, s) {
+  var g = ohm.grammar(fs.readFileSync(path.join(__dirname, 'es6.ohm')).toString(), ns);
+  var semantics = g.extendSemantics(s).extendAttribute('modifiedSource', {
+    ArrowFunction: function(params, _, body) {
+      return 'function ' + params.asES5 + '{ ' + body.asES5 + '}';
+    }
+  });
+
+  return {
+    grammar: g,
+    semantics: semantics
+  };
+};
