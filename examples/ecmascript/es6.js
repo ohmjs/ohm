d9faa42e28da4dbd8717085d0536a1a614b8ba42
Patrick Dubroy <pdubroy@gmail.com>
2015-06-18 18:15:36 +0200

    Fix arrow functions in ES6 grammar.

--- examples/ecmascript/es6.js
+++ examples/ecmascript/es6.js
@@ -9,7 +9,10 @@ module.exports = function(ohm, ns, s) {
   var g = ohm.grammar(fs.readFileSync(path.join(__dirname, 'es6.ohm')).toString(), ns);
   var semantics = g.extendSemantics(s).extendAttribute('modifiedSource', {
     ArrowFunction: function(params, _, body) {
-      return 'function ' + params.asES5 + '{ ' + body.asES5 + '}';
+      return 'function ' + params.asES5 + ' { ' + body.asES5 + ' }.bind(this)';
+    },
+    ArrowParameters_unparenthesized: function(id) {
+      return '(' + id.asES5 + ')';
     }
   });
 
