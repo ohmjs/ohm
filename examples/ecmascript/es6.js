5e587e9161e02c95895c0a90bda9abd072e245a4
Patrick Dubroy <pdubroy@gmail.com>
2015-06-19 11:57:21 +0200

    ES6 example: make arrow functions closer to correct.

--- examples/ecmascript/es6.js
+++ examples/ecmascript/es6.js
@@ -5,16 +5,38 @@
 var fs = require('fs');
 var path = require('path');
 
+// Semantic actions for the `mentionsThis` attribute, which returns true for a node
+// if the `this` keyword appears anywhere in the node's subtree, and otherwise false.
+var mentionsThisActions = {
+  'this': function(_) { return true; },
+  _terminal: function() { return false; },
+  _default: anyNodesMentionThis,
+  _iter: anyNodesMentionThis
+};
+
+function anyNodesMentionThis(nodes) {
+  return nodes.some(function(n) { return n.mentionsThis; });
+}
+
+var modifiedSourceActions = {
+  ArrowFunction: function(params, _, body) {
+    var source = 'function ' + params.asES5 + ' ' + body.asES5;
+    // Only use `bind` if necessary.
+    return body.mentionsThis ? source + '.bind(this)' : source;
+  },
+  ArrowParameters_unparenthesized: function(id) {
+    return '(' + id.asES5 + ')';
+  },
+  ConciseBody_noBraces: function(exp) {
+    return '{ return ' + exp.asES5 + ' }';
+  }
+};
+
 module.exports = function(ohm, ns, s) {
   var g = ohm.grammar(fs.readFileSync(path.join(__dirname, 'es6.ohm')).toString(), ns);
-  var semantics = g.extendSemantics(s).extendAttribute('modifiedSource', {
-    ArrowFunction: function(params, _, body) {
-      return 'function ' + params.asES5 + ' { ' + body.asES5 + ' }.bind(this)';
-    },
-    ArrowParameters_unparenthesized: function(id) {
-      return '(' + id.asES5 + ')';
-    }
-  });
+  var semantics = g.extendSemantics(s);
+  semantics.extendAttribute('modifiedSource', modifiedSourceActions);
+  semantics.addAttribute('mentionsThis', mentionsThisActions);
 
   return {
     grammar: g,
