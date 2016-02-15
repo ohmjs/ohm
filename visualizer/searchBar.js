7217039944f5e8b98bbac47501d7c05503c6c577
Patrick Dubroy <pdubroy@gmail.com>
2016-02-15 11:37:11 +0100

    Viz: Handle Cmd-F etc. properly when search bar has focus.

--- visualizer/searchBar.js
+++ visualizer/searchBar.js
@@ -13,36 +13,13 @@ var searchBar = (function() {  // eslint-disable-line no-unused-vars
     }
   }
 
-  // An implementation of CodeMirror's `openDialog` API, but specialized for use
-  // as the search box. As such, it might not be very well suited to other purposes.
-  CodeMirror.defineExtension('openDialog', function(template, callback, opts) {
-    var ENTER_KEY = 13;
-    var ESC_KEY = 27;
-
-    var editor = this;
-
-    if (template.indexOf('Search:') !== 0) {
-      throw new Error('No dialog for template ' + template);
-    }
-
-    // Re-use the existing footer if it's visible, or create a new one.
-    var container = ancestorWithClassName(editor.getWrapperElement(), 'flex-fix').parentNode;
-    var footer = container.querySelector('.footer');
-    var reused = true;
-    if (!footer) {
-      footer = $('#protos .footer').cloneNode(true);
-      container.appendChild(footer);
-      footer.removeAttribute('hidden');
-      reused = false;
-    }
-
-    var closeButton = footer.querySelector('.closeButton');
+  function installEventHandlers(editor, footer, findCallback) {
     var input = footer.querySelector('input[type=search]');
 
     // Install a temporary keymap while the toolbar is open, which makes Cmd-F
     // refocus the search bar rather than starting a new search.
-    var keyMap = {};
-    keyMap['Cmd-F'] = function(cm) {
+    var tempKeyMap = {fallthrough: 'default'};
+    tempKeyMap['Cmd-F'] = tempKeyMap['Ctrl-F'] = function doFind(cm) {
       var selection = cm.getSelection();
 
       // If there's no selection or it's the same as the current search, refocus the search bar.
@@ -53,32 +30,72 @@ var searchBar = (function() {  // eslint-disable-line no-unused-vars
         cm.execCommand('findPersistent');
       }
     };
+    editor.addKeyMap(tempKeyMap);
 
-    if (!reused) {
-      var closeFooter = function() {
-        footer.parentNode.removeChild(footer);
-        editor.execCommand('clearSearch');
-        editor.removeKeyMap(keyMap);
+    // Handles find-related keys when the search bar has focus. `binding` is the result
+    // of looking up the key in `tempKeyMap`. Returns `true` if the key was handled, and
+    // `false` otherwise.
+    var handleKey = function(binding) {
+      if (typeof binding === 'function') {
+        binding(editor);
+        return true;
+      } else if (typeof binding === 'string' && binding.indexOf('find') === 0) {
+        editor.execCommand(binding);
+        return true;
+      }
+      return false;
+    };
+
+    var closeFooter = function() {
+      footer.parentNode.removeChild(footer);
+      editor.execCommand('clearSearch');
+      editor.removeKeyMap(tempKeyMap);
+      editor.focus();
+    };
+
+    // Handler for keydown events in the search bar.
+    footer.onkeydown = function(e) {
+      var keyName = CodeMirror.keyName(e);
+      if (keyName === 'Esc') {
+        closeFooter();
         editor.focus();
-      };
-      closeButton = footer.querySelector('.closeButton');
-      closeButton.onclick = closeFooter;
-      editor.addKeyMap(keyMap);
+      } else if (e.target === input && keyName === 'Enter') {
+        findCallback(input.value, e);
+      } else if (CodeMirror.lookupKey(keyName, tempKeyMap, handleKey) === 'handled') {
+        e.preventDefault();
+      }
+    };
 
-      input.onkeydown = function(e) {
-        if (e.keyCode === ESC_KEY) {
-          closeFooter();
-          editor.focus();
-        } else if (e.keyCode === ENTER_KEY) {
-          callback(input.value, e);
-        }
-      };
+    var closeButton = footer.querySelector('.closeButton');
+    closeButton.onclick = closeFooter;
+  }
+
+  // An implementation of CodeMirror's `openDialog` API, but specialized for use
+  // as the search box. As such, it might not be very well suited to other purposes.
+  CodeMirror.defineExtension('openDialog', function(template, callback, opts) {
+    var editor = this;
+
+    if (template.indexOf('Search:') !== 0) {
+      throw new Error('No dialog for template ' + template);
     }
+
+    // Re-use the existing footer if it's visible, or create a new one.
+    var container = ancestorWithClassName(editor.getWrapperElement(), 'flex-fix').parentNode;
+    var footer = container.querySelector('.footer');
+    if (!footer) {
+      footer = $('#protos .footer').cloneNode(true);
+      container.appendChild(footer);
+      footer.removeAttribute('hidden');
+      installEventHandlers(editor, footer, callback);
+    }
+
+    var closeButton = footer.querySelector('.closeButton');
+    var input = footer.querySelector('input[type=search]');
+
     if (opts.value) {
       input.value = opts.value;
     }
     input.select();
-
     return closeButton.onclick;
   });
 
