2c10ebbe90460a2b4527d0982bcdeb6b8a2a419b
Meixian Li <lmeixian@gmail.com>
2016-03-11 10:06:10 -0800

    rename result/ans to cstNode for trace

--- src/Trace.js
+++ src/Trace.js
@@ -61,17 +61,17 @@ function asEscapedString(obj) {
 
 // ----------------- Trace -----------------
 
-function Trace(inputStream, pos, expr, result, optChildren) {
+function Trace(inputStream, pos, expr, cstNode, optChildren) {
   this.children = optChildren || [];
   this.expr = expr;
-  if (result) {
+  if (cstNode) {
     this.interval = new Interval(inputStream, pos, inputStream.pos);
-    this.cst = result;
+    this.cstNode = cstNode;
   }
   this.isLeftRecursive = false;
   this.pos = pos;
   this.inputStream = inputStream;
-  this.succeeded = !!result;
+  this.succeeded = !!cstNode;
 }
 
 // A value that can be returned from visitor functions to indicate that a
@@ -83,7 +83,7 @@ Object.defineProperty(Trace.prototype, 'displayString', {
 });
 
 Trace.prototype.cloneWithExpr = function(expr) {
-  var ans = new Trace(this.inputStream, this.pos, expr, this.cst, this.children);
+  var ans = new Trace(this.inputStream, this.pos, expr, this.cstNode, this.children);
   ans.isLeftRecursive = this.isLeftRecursive;
   ans.isMemoized = true;
   return ans;
