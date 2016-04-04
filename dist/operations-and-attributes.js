3b2ae99e362000cd5c109451b25f4be7d7712810
Alessandro Warth <alexwarth@gmail.com>
2016-04-04 11:44:50 -0700

    somehow these files were left out of my last commit (my bad)

--- dist/operations-and-attributes.js
+++ dist/operations-and-attributes.js
@@ -1,14 +1,15 @@
 var ohm = require('..');
 module.exports = ohm.makeRecipe(function() {
   var decl = this.newGrammar("OperationsAndAttributes")
-    .withSource("OperationsAndAttributes {\n\n  NameNoFormals =\n    name\n\n  NameAndFormals =\n    name Formals?\n\n  Formals\n    = \"(\" ListOf<name, \",\"> \")\"\n\n  name  (a name)\n    = nameFirst nameRest*\n\n  nameFirst\n    = \"_\"\n    | letter\n\n  nameRest\n    = \"_\"\n    | alnum\n\n}")
+    .withSource("OperationsAndAttributes {\n\n  AttributeSignature =\n    name\n\n  OperationSignature =\n    name Formals?\n\n  Formals\n    = \"(\" ListOf<name, \",\"> \")\"\n\n  name  (a name)\n    = nameFirst nameRest*\n\n  nameFirst\n    = \"_\"\n    | letter\n\n  nameRest\n    = \"_\"\n    | alnum\n\n}")
     .withDefaultStartRule("AttributeSignature")
   return decl
-    .define("AttributeSignature", [], this.app("name").withInterval(decl.sourceInterval(49, 53)))
-    .define("OperationSignature", [], this.seq(this.app("name").withInterval(decl.sourceInterval(78, 82)), this.opt(this.app("Formals").withInterval(decl.sourceInterval(83, 90))).withInterval(decl.sourceInterval(83, 91))).withInterval(decl.sourceInterval(78, 91)))
-    .define("Formals", [], this.seq(this.prim("(").withInterval(decl.sourceInterval(109, 112)), this.app("ListOf", [this.app("name").withInterval(decl.sourceInterval(120, 124)), this.prim(",").withInterval(decl.sourceInterval(126, 129))]).withInterval(decl.sourceInterval(113, 130)), this.prim(")").withInterval(decl.sourceInterval(131, 134))).withInterval(decl.sourceInterval(109, 134)))
-    .define("name", [], this.seq(this.app("nameFirst").withInterval(decl.sourceInterval(159, 168)), this.star(this.app("nameRest").withInterval(decl.sourceInterval(169, 177))).withInterval(decl.sourceInterval(169, 178))).withInterval(decl.sourceInterval(159, 178)), "a name")
-    .define("nameFirst", [], this.alt(this.prim("_").withInterval(decl.sourceInterval(198, 201)), this.app("letter").withInterval(decl.sourceInterval(208, 214))).withInterval(decl.sourceInterval(198, 214)))
-    .define("nameRest", [], this.alt(this.prim("_").withInterval(decl.sourceInterval(233, 236)), this.app("alnum").withInterval(decl.sourceInterval(243, 248))).withInterval(decl.sourceInterval(233, 248)))
+    .define("AttributeSignature", [], this.app("name").withInterval(decl.sourceInterval(54, 58)))
+    .define("OperationSignature", [], this.seq(this.app("name").withInterval(decl.sourceInterval(87, 91)), this.opt(this.app("Formals").withInterval(decl.sourceInterval(92, 99))).withInterval(decl.sourceInterval(92, 100))).withInterval(decl.sourceInterval(87, 100)))
+    .define("Formals", [], this.seq(this.prim("(").withInterval(decl.sourceInterval(118, 121)), this.app("ListOf", [this.app("name").withInterval(decl.sourceInterval(129, 133)), this.prim(",").withInterval(decl.sourceInterval(135, 138))]).withInterval(decl.sourceInterval(122, 139)), this.prim(")").withInterval(decl.sourceInterval(140, 143))).withInterval(decl.sourceInterval(118, 143)))
+    .define("name", [], this.seq(this.app("nameFirst").withInterval(decl.sourceInterval(168, 177)), this.star(this.app("nameRest").withInterval(decl.sourceInterval(178, 186))).withInterval(decl.sourceInterval(178, 187))).withInterval(decl.sourceInterval(168, 187)), "a name")
+    .define("nameFirst", [], this.alt(this.prim("_").withInterval(decl.sourceInterval(207, 210)), this.app("letter").withInterval(decl.sourceInterval(217, 223))).withInterval(decl.sourceInterval(207, 223)))
+    .define("nameRest", [], this.alt(this.prim("_").withInterval(decl.sourceInterval(242, 245)), this.app("alnum").withInterval(decl.sourceInterval(252, 257))).withInterval(decl.sourceInterval(242, 257)))
     .build();
 });
+
