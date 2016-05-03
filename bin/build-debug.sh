7a6b1931aa5c6f453a11fff9f97ce69ae4d00b81
Justin Chase <justin.m.chase@gmail.com>
2016-05-03 02:19:21 -0500

    Build on windows successfully
    When npm runs on windows (even from git bash) it executes scripts as CMD
    scripts. Using `$var` in these scripts causes issues on Windows.
    
    Bash can be invoked however, so moving these scripts into shell files in
    the bin directory allows the scripts to be completely bash while still
    working on windows.

---
+++ bin/build-debug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ $npm_package_browserify_options ]; then
+    options=$npm_package_browserify_options
+else
+    options=$NPM_PACKAGE_BROWSERIFY_OPTIONS
+fi
+
+browserify $options
