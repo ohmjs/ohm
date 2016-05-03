9789c6c7e3daba7aa8fb0578fd660b9fb7fe188d
Justin Chase <justin.m.chase@gmail.com>
2016-05-03 11:32:29 -0500

    Cleanup the build-debug and watch script a bit.

--- bin/watch.sh
+++ bin/watch.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ $npm_package_browserify_options ]; then
-  options=$npm_package_browserify_options
-else
-  options=$NPM_PACKAGE_BROWSERIFY_OPTIONS
-fi
+options=${NPM_PACKAGE_BROWSERIFY_OPTIONS:-$npm_package_browserify_options} 
 
 watchify -v $options
