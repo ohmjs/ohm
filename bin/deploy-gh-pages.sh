c106a7fc08ed8dd1917cb82506cd8542b097b238
Patrick Dubroy <pdubroy@gmail.com>
2016-07-15 14:29:04 +0200

    deploy-gh-pages.sh: Move visualizer/ dir to editor/.

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -17,6 +17,7 @@ fi
 
 git pull --ff-only --no-stat
 cp -r "$ROOT/doc" "$ROOT/dist" "$ROOT/visualizer" .
-git add doc dist visualizer
+mv visualizer editor  # Temporary until main repo is changed.
+git add doc dist editor visualizer
 git commit -m "Update from master@${OHM_REV}"
 git push origin master
