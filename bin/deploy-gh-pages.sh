d29d0ade3242620ca9102aa76937a2eb0706d679
Patrick Dubroy <pdubroy@gmail.com>
2016-07-16 10:15:18 +0200

    More bug fixes for deploy-gh-pages script.

--- bin/deploy-gh-pages.sh
+++ bin/deploy-gh-pages.sh
@@ -23,8 +23,8 @@ if ! git rev-parse --quiet --verify master > /dev/null; then
 fi
 
 git pull --ff-only --no-stat
-cp -r "$ROOT/doc" "$ROOT/dist" "$ROOT/visualizer" .
-mv visualizer editor  # Temporary until main repo is changed.
+cp -r "$ROOT/doc" "$ROOT/dist" .
+cp -r "$ROOT/visualizer/" ./editor  # Temporary until main repo is changed.
 git add doc dist editor
 git commit -m "Update from master@${OHM_REV}"
 git push origin master
