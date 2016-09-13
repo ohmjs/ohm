660fd4b141200d968aab9e304b0ba36621eb7963
Patrick Dubroy <pdubroy@gmail.com>
2016-09-13 16:47:21 +0200

    Revert changes to pexprs-getArity, as it causes perf regression.

--- src/pexprs-getArity.js
+++ src/pexprs-getArity.js
@@ -4,64 +4,48 @@
 // Imports
 // --------------------------------------------------------------------
 
-var PExpr = require('./pexprs').PExpr;
-var pexprVisitors = require('./pexpr-visitors');
-
-// --------------------------------------------------------------------
-// Private Stuff
-// --------------------------------------------------------------------
-
-var actions = {
-  Alt: function(terms) {
-    // This is ok b/c all terms must have the same arity -- this property is
-    // checked by the Grammar constructor.
-    return terms.length === 0 ? 0 : terms[0].getArity();
-  },
-  Apply: function(args) {
-    return 1;
-  },
-  Iter: function(expr) {
-    return expr.getArity();
-  },
-  Not: function(expr) {
-    return 0;
-  },
-  Seq: function(factors) {
-    var arity = 0;
-    for (var idx = 0; idx < factors.length; idx++) {
-      arity += factors[idx].getArity();
-    }
-    return arity;
-  }
-};
-
-actions.Extend = actions.Alt;
-
-actions.Lookahead =
-actions.Lex =
-actions.Opt =
-actions.Plus =
-actions.Star = function(expr) {
-  return expr.getArity();
-};
-
-actions.any =
-actions.end =
-actions.Terminal =
-actions.Range =
-actions.Param =
-actions.UnicodeChar = function() {
-  return 1;
-};
+var common = require('./common');
+var pexprs = require('./pexprs');
 
 // --------------------------------------------------------------------
 // Operations
 // --------------------------------------------------------------------
 
-pexprVisitors.addOperation('getArity()', actions);
+pexprs.PExpr.prototype.getArity = common.abstract('getArity');
 
-// For now, install getArity as a regular method on PExpr, so that other code
-// does not need to be aware of pexprVisitors.
-PExpr.prototype.getArity = function() {
-  return pexprVisitors.wrap(this).getArity();
+pexprs.any.getArity =
+pexprs.end.getArity =
+pexprs.Terminal.prototype.getArity =
+pexprs.Range.prototype.getArity =
+pexprs.Param.prototype.getArity =
+pexprs.Apply.prototype.getArity =
+pexprs.UnicodeChar.prototype.getArity = function() {
+  return 1;
+};
+
+pexprs.Alt.prototype.getArity = function() {
+  // This is ok b/c all terms must have the same arity -- this property is
+  // checked by the Grammar constructor.
+  return this.terms.length === 0 ? 0 : this.terms[0].getArity();
+};
+
+pexprs.Seq.prototype.getArity = function() {
+  var arity = 0;
+  for (var idx = 0; idx < this.factors.length; idx++) {
+    arity += this.factors[idx].getArity();
+  }
+  return arity;
+};
+
+pexprs.Iter.prototype.getArity = function() {
+  return this.expr.getArity();
+};
+
+pexprs.Not.prototype.getArity = function() {
+  return 0;
+};
+
+pexprs.Lookahead.prototype.getArity =
+pexprs.Lex.prototype.getArity = function() {
+  return this.expr.getArity();
 };
