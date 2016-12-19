b63aa8417024c91690d422062e2689e3a7915942
Alessandro Warth <alexwarth@gmail.com>
2016-12-19 10:38:25 -0800

    remove MatchResult.prototype.getDiscardedSpaces()

--- src/MatchResult.js
+++ src/MatchResult.js
@@ -5,7 +5,6 @@
 // --------------------------------------------------------------------
 
 var common = require('./common');
-var nodes = require('./nodes');
 var util = require('./util');
 var Interval = require('./Interval');
 
@@ -63,70 +62,6 @@ MatchResult.prototype.getRightmostFailures = function() {
     this._rightmostFailures = matchResultWithFailures.getRightmostFailures();
   }
   return this._rightmostFailures;
-};
-
-// Returns a `MatchResult` that can be fed into operations or attributes that care
-// about the whitespace that was implicitly skipped over by syntactic rules. This
-// is useful for doing things with comments, e.g., syntax highlighting.
-MatchResult.prototype.getDiscardedSpaces = function() {
-  if (this.failed()) {
-    throw new Error('Cannot get discarded spaces of failed match result');
-  }
-
-  // Populate the memo table with the info for this match result, in case
-  // it's been invalidated by subsequent matches.
-  this.matcher.setInput(this.input);
-  this.matcher._match(this.startExpr, false);
-
-  var grammar = this.matcher.grammar;
-  var memoTable = this.matcher.memoTable;
-
-  var intervals = [new Interval(this.input, 0, this.input.length)];
-
-  // Subtract the interval of each terminal from the set of intervals above.
-  var s = grammar.createSemantics().addOperation('subtractTerminals', {
-    _nonterminal: function(children) {
-      children.forEach(function(child) {
-        child.subtractTerminals();
-      });
-    },
-    _terminal: function() {
-      var t = this;
-      intervals = intervals
-          .map(function(interval) { return interval.minus(t.source); })
-          .reduce(function(xs, ys) { return xs.concat(ys); }, []);
-    }
-  });
-  s(this).subtractTerminals();
-
-  // Now, at the beginning of each interval, there should be a memo table entry for the 'spaces'
-  // rule, representing the node that was discarded. Collect those along with their offsets.
-  var offsets = intervals.map(function(interval) {
-    return interval.startIdx;
-  });
-  var discardedNodes = offsets.map(function(pos) {
-    var posInfo = memoTable[pos];
-    var memoRec = posInfo.memo.spaces;
-    return memoRec.value;
-  });
-
-  // Rather than return a bunch of CST nodes and make the caller of this method loop over them,
-  // we can construct a single CST node that is the parent of all of the discarded nodes. An
-  // `IterationNode` is the obvious choice for this.
-  var iter = new nodes.IterationNode(grammar, discardedNodes, offsets, -1, false);
-
-  // But remember that a CST node can't be used directly by clients. What we really need to return
-  // from this method is a successful `MatchResult` that can be used with the clients' semantics.
-  // We already have one -- `this` -- but it's got a different CST node inside. So we create a new
-  // object that delegates to `this`, and override its `_cst` and `_cstOffset` properties.
-  var r = Object.create(this);
-  r._cst = iter;
-  r._cstOffset = 0;
-
-  // We also override its `getDiscardedSpaces` method, in case someone decides to call it.
-  r.getDiscardedSpaces = function() { return r; };
-
-  return r;
 };
 
 MatchResult.prototype.toString = function() {
