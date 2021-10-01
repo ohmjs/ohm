2f677a4e751a47c7e07911268628990cd67a0135
Patrick Dubroy <pdubroy@gmail.com>
2021-10-01 14:32:05 +0200

    More renames

--- packages/cli/src/commands/generateBundles/index.test.mjs
+++ packages/cli/src/commands/generateBundles/index.test.mjs
@@ -3,41 +3,41 @@ import {dirname} from 'path';
 import requireFromString from 'require-from-string';
 import {fileURLToPath} from 'url';
 
-import generateRecipesCommand from './index.js';
+import generateBundlesCommand from './index.js';
 
-const generateRecipes = generateRecipesCommand.action;
+const generateBundles = generateBundlesCommand.action;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const baseOpts = {cwd: `${__dirname}/testdata`, dryRun: true};
 
 test('basic', t => {
-  let plan = generateRecipes(['**/*.ohm'], baseOpts);
+  let plan = generateBundles(['**/*.ohm'], baseOpts);
   t.deepEqual(Object.keys(plan.filesToWrite), [
-    'arithmetic.ohm-recipe.js',
-    'e/f/g.ohm-recipe.js'
+    'arithmetic.ohm-bundle.js',
+    'e/f/g.ohm-bundle.js'
   ]);
 
-  plan = generateRecipes(['**/*.ohm'], {...baseOpts, withTypes: true});
+  plan = generateBundles(['**/*.ohm'], {...baseOpts, withTypes: true});
   t.deepEqual(Object.keys(plan.filesToWrite), [
-    'arithmetic.ohm-recipe.js',
-    'arithmetic.ohm-recipe.d.ts',
-    'e/f/g.ohm-recipe.js',
-    'e/f/g.ohm-recipe.d.ts'
+    'arithmetic.ohm-bundle.js',
+    'arithmetic.ohm-bundle.d.ts',
+    'e/f/g.ohm-bundle.js',
+    'e/f/g.ohm-bundle.d.ts'
   ]);
 
-  const gDecl = plan.filesToWrite['e/f/g.ohm-recipe.d.ts'];
+  const gDecl = plan.filesToWrite['e/f/g.ohm-bundle.d.ts'];
 
   // Check that the declaration looks vaguely correct.
   t.true(gDecl.includes('export interface GGrammar'));
   t.true(gDecl.includes('export interface GSemantics'));
 
-  plan = generateRecipes(['*.ohm'], {...baseOpts, withTypes: true});
+  plan = generateBundles(['*.ohm'], {...baseOpts, withTypes: true});
   t.deepEqual(Object.keys(plan.filesToWrite), [
-    'arithmetic.ohm-recipe.js',
-    'arithmetic.ohm-recipe.d.ts'
+    'arithmetic.ohm-bundle.js',
+    'arithmetic.ohm-bundle.d.ts'
   ]);
 
-  const arithmeticDecl = plan.filesToWrite['arithmetic.ohm-recipe.d.ts'];
+  const arithmeticDecl = plan.filesToWrite['arithmetic.ohm-bundle.d.ts'];
 
   t.true(arithmeticDecl.includes('export interface ArithmeticGrammar'));
   t.true(arithmeticDecl.includes('export interface ArithmeticSemantics'));
@@ -45,29 +45,28 @@ test('basic', t => {
 });
 
 test('arithmetic grammar with no types', t => {
-  const {filesToWrite} = generateRecipes(['arithmetic.ohm'], baseOpts);
-  t.deepEqual(Object.keys(filesToWrite), ['arithmetic.ohm-recipe.js']);
-  t.snapshot(filesToWrite['arithmetic.ohm-recipe.js']);
+  const {filesToWrite} = generateBundles(['arithmetic.ohm'], baseOpts);
+  t.deepEqual(Object.keys(filesToWrite), ['arithmetic.ohm-bundle.js']);
+  t.snapshot(filesToWrite['arithmetic.ohm-bundle.js']);
 
-  const g = requireFromString(filesToWrite['arithmetic.ohm-recipe.js']);
+  const g = requireFromString(filesToWrite['arithmetic.ohm-bundle.js']);
   t.true(g.match('1+2').succeeded());
 });
 
 test('arithmetic grammar with types', t => {
-  const {filesToWrite} = generateRecipes(['arithmetic.ohm'], {...baseOpts, withTypes: true});
+  const {filesToWrite} = generateBundles(['arithmetic.ohm'], {...baseOpts, withTypes: true});
   t.deepEqual(Object.keys(filesToWrite), [
-    'arithmetic.ohm-recipe.js',
-    'arithmetic.ohm-recipe.d.ts'
+    'arithmetic.ohm-bundle.js',
+    'arithmetic.ohm-bundle.d.ts'
   ]);
-  t.snapshot(filesToWrite['arithmetic.ohm-recipe.d.ts']);
+  t.snapshot(filesToWrite['arithmetic.ohm-bundle.d.ts']);
 });
 
 test('multiple grammars', t => {
-  const {filesToWrite} = generateRecipes(['e/f/g.ohm'], baseOpts);
-  t.deepEqual(Object.keys(filesToWrite), ['e/f/g.ohm-recipe.js']);
-  console.log(filesToWrite['e/f/g.ohm-recipe.js']);
+  const {filesToWrite} = generateBundles(['e/f/g.ohm'], baseOpts);
+  t.deepEqual(Object.keys(filesToWrite), ['e/f/g.ohm-bundle.js']);
 
-  const ns = requireFromString(filesToWrite['e/f/g.ohm-recipe.js']);
+  const ns = requireFromString(filesToWrite['e/f/g.ohm-bundle.js']);
   t.true(ns.G.match('G').succeeded());
   t.true(ns.G2.match('G2').succeeded());
 });
