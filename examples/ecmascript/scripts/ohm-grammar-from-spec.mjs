d598b3b1b073cda24bd64d4af73dff7a5284f03d
Patrick Dubroy <pdubroy@gmail.com>
2021-12-05 21:25:19 +0100

    proper lexical rules - it actually parses some simple stuff!

--- examples/ecmascript/scripts/ohm-grammar-from-spec.mjs
+++ examples/ecmascript/scripts/ohm-grammar-from-spec.mjs
@@ -1,3 +1,4 @@
+import assert from 'assert';
 import {readFileSync, writeFileSync} from 'fs';
 import {parse} from 'node-html-parser';
 import ohm from 'ohm-js';
@@ -9,6 +10,10 @@ console.log(
     .querySelectorAll('emu-grammar[type="definition"]:not([example])')
     .map(node => node.innerHTML)
 );
+
+NEXT --------------------------------
+- create some tests of some basic stuff
+- rewrite name of lexical rules (lowercase)
 */
 
 const INPUT_FILENAME = 'es2015.grammar';
@@ -33,6 +38,18 @@ const literalToOhm = {
   PS: '\\u2029'
 };
 
+const ruleOverrides = {
+  unicodeIDStart: 'letter /* fixme */',
+  unicodeIDContinue: 'letter | digit /* fixme */',
+  sourceCharacter: 'any'
+};
+
+const lexicalRuleName = str => str[0].toLowerCase() + str.slice(1);
+const syntacticRuleName = str => {
+  assert(str[0] === str[0].toUpperCase());
+  return str;
+};
+
 const semantics = g.createSemantics();
 semantics.addOperation('toOhm()', {
   Productions(productionIter) {
@@ -40,15 +57,21 @@ semantics.addOperation('toOhm()', {
     for (const param of new Set(this.allParameters)) {
       rules.push(...[`with${param} = /* fixme */`, `no${param} = /* fixme */`]);
     }
-    return ['G {', ...rules, '}'].join('\n\n  ');
+    const prettyRules = ['', ...rules].join('\n\n  ');
+    return `ES2015 {${prettyRules}\n}`;
   },
   Production_lexical(nonterminal, _, rhs) {
-    return `${nonterminal.sourceString} = ${rhs.toOhm()}`;
+    const ruleName = lexicalRuleName(nonterminal.sourceString);
+    const op = ruleName === 'hexDigit' ? ':=' : '=';
+    const body = ruleOverrides[ruleName] || rhs.toOhm();
+    return `${ruleName} ${op} ${body}`;
   },
   Production_syntactic(nonterminal, parameterListOpt, _, rhs) {
+    const ruleName = syntacticRuleName(nonterminal.sourceString);
     const parameterList = parameterListOpt.child(0);
     const params = parameterList ? parameterList.toOhm() : '';
-    return `${nonterminal.sourceString}${params} = ${rhs.toOhm()}`;
+    const body = ruleOverrides[ruleName] || rhs.toOhm();
+    return `${ruleName}${params} = ${body}`;
   },
   ParameterList(_open, listOfParameter, _close) {
     const params = listOfParameter.asIteration().children.map(c => c.toOhm());
@@ -75,7 +98,7 @@ semantics.addOperation('toOhm()', {
     return termIter.children.map(c => c.toOhm()).join(' ');
   },
   application_basic(nonterminal) {
-    return nonterminal.sourceString;
+    return nonterminal.toOhm();
   },
   term_opt(application, _) {
     return `${application.toOhm()}?`;
@@ -93,7 +116,7 @@ semantics.addOperation('toOhm()', {
     return `/* FIXME Assertion: ${this.sourceString} */`;
   },
   AssertionContents_noSymbolHere(_no, nonterminal, _here) {
-    return `~${nonterminal.sourceString}`;
+    return `~${nonterminal.toOhm()}`;
   },
   AssertionContents_paramSet(_, param) {
     return `guard${param.sourceString}`;
@@ -105,11 +128,11 @@ semantics.addOperation('toOhm()', {
     return `/* FIXME Assertion: ${this.sourceString} */`;
   },
   application_withCondition(nonterminal, butNotCondition) {
-    return `${butNotCondition.toOhm()} ${nonterminal.sourceString}`;
+    return `${butNotCondition.toOhm()} ${nonterminal.toOhm()}`;
   },
   application_withArgs(nonterminal, _open, applyListOfArgument, _close) {
     const listOfArgument = applyListOfArgument.child(0);
-    const ruleName = nonterminal.sourceString;
+    const ruleName = nonterminal.toOhm();
     const ohmArgs = getOhmArgs(
       this.context.productions,
       ruleName,
@@ -118,7 +141,7 @@ semantics.addOperation('toOhm()', {
     return `${ruleName}<${ohmArgs.join(', ')}>`;
   },
   application_basic(nonterminal) {
-    const ruleName = nonterminal.sourceString;
+    const ruleName = nonterminal.toOhm();
     const ohmArgs = getOhmArgs(this.context.productions, ruleName, []);
     if (ohmArgs.length > 0) {
       return `${ruleName}<${ohmArgs.join(', ')}>`;
@@ -139,7 +162,14 @@ semantics.addOperation('toOhm()', {
     return `~(${terms.join(' | ')})`;
   },
   nonterminal(_, _2) {
-    return this.sourceString;
+    const {sourceString} = this;
+    const root = this.context.productions;
+    if (root.productionsByName.has(sourceString)) {
+      return sourceString;
+    }
+    const lexicalName = lexicalRuleName(sourceString);
+    assert(root.productionsByName.has(lexicalName));
+    return lexicalName;
   },
   terminal_backtick(_) {
     return '"`"';
@@ -212,18 +242,18 @@ semantics.addAttribute('productionsByName', {
   Productions(productionIter) {
     return new Map(
       productionIter.children.map(prod => {
-        return [prod.productionName, prod];
+        return [prod.ruleNameForProduction, prod];
       })
     );
   }
 });
 
-semantics.addAttribute('productionName', {
+semantics.addAttribute('ruleNameForProduction', {
   Production_lexical(nonterminal, _, rhs) {
-    return nonterminal.sourceString;
+    return lexicalRuleName(nonterminal.sourceString);
   },
   Production_syntactic(nonterminal, parameterListOpt, _, rhs) {
-    return nonterminal.sourceString;
+    return syntacticRuleName(nonterminal.sourceString);
   }
 });
 
@@ -267,8 +297,12 @@ const ohmGrammar = root.toOhm();
 writeFileSync(`${INPUT_FILENAME}.ohm`, ohmGrammar, 'utf-8');
 //console.log(ohmGrammar);
 try {
-  ohm.grammar(ohmGrammar);
+  const es6 = ohm.grammar(ohmGrammar);
   console.log('grammar parsed successfully!');
+  assert(es6.match('const x = 3;', 'Script'));
+  assert(es6.match('const x = () => 3;', 'Script'));
+  assert(es6.match('class Foo { constructor() {}}; const f = new Foo();', 'Script'));
+  console.log('tests pass!');
 } catch (e) {
   console.log(e.message);
 }
