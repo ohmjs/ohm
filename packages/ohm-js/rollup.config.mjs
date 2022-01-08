12f2daa848c654bc1d8c99b66ff4b4d91595dca4
Patrick Dubroy <pdubroy@gmail.com>
2022-01-08 15:25:40 +0100

    Add triple-slash directive to help Deno find the types

--- packages/ohm-js/rollup.config.mjs
+++ packages/ohm-js/rollup.config.mjs
@@ -6,7 +6,10 @@ export default {
   input: 'index.mjs',
   output: {
     file: 'dist/ohm.esm.js',
-    format: 'es'
+    format: 'es',
+    // Help Deno to find the types when the module is loaded from a server.
+    // See https://deno.land/manual/typescript/types#using-the-triple-slash-reference-directive
+    banner: '/// <reference types="../index.d.ts" />'
   },
-  plugins: [resolve(), commonjs(), json()]
+  plugins: [resolve(), commonjs(), json()],
 };
