af5e05af2c2ff5282047f53c608f38b188adf19c
Patrick Dubroy <pdubroy@gmail.com>
2022-11-03 10:09:27 +0100

    Fix exports for Ohm extras (including .d.ts)

--- packages/ohm-js/rollup.config.js
+++ packages/ohm-js/rollup.config.js
@@ -5,17 +5,16 @@ export default [
     input: 'index.mjs',
     output: [
       {file: 'dist/ohm.cjs', format: 'cjs', exports: 'default'},
-      {file: 'dist/ohm.js', format: 'iife', name: 'ohm'},
+      {file: 'dist/ohm.js', format: 'umd', name: 'ohm'},
     ],
     plugins: [resolve()],
   },
   {
     input: 'extras/index.mjs',
-    output: {
-      exports: 'named',
-      file: 'dist/ohm-extras.cjs',
-      format: 'cjs',
-    },
+    output: [
+      {file: 'dist/ohm-extras.cjs', format: 'cjs', exports: 'named'},
+      {file: 'dist/ohm-extras.js', format: 'umd', name: 'ohmExtras'},
+    ],
     plugins: [resolve()],
   },
 ];
