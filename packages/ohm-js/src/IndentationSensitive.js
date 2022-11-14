787d0f46841dd39eb9a4e57c271c7d1020e0aa5c
Patrick Dubroy <pdubroy@gmail.com>
2022-11-14 06:37:44 +0100

    Add InputStreamWithIndentation

--- packages/ohm-js/src/IndentationSensitive.js
+++ packages/ohm-js/src/IndentationSensitive.js
@@ -4,9 +4,53 @@ import {Failure} from '../src/Failure.js';
 import {TerminalNode} from '../src/nodes.js';
 import {PExpr} from '../src/pexprs.js';
 import {findIndentation} from './findIndentation.js';
+import {InputStream} from './InputStream.js';
 
 const INDENT_DESCRIPTION = 'an indented block';
 const DEDENT_DESCRIPTION = 'a dedent';
+
+// A sentinel value that is out of range for both charCodeAt() and codePointAt().
+const INVALID_CODE_POINT = 0x10ffff + 1;
+
+class InputStreamWithIndentation extends InputStream {
+  constructor(state) {
+    super(state.input);
+    this.state = state;
+  }
+
+  _indentationAt(pos) {
+    return this.state.userData[pos] || 0;
+  }
+
+  atEnd() {
+    return super.atEnd() && this._indentationAt(this.pos) === 0;
+  }
+
+  next() {
+    // TODO: Is this right? It prevents `any` from consuming indentation.
+    if (this._indentationAt(this.pos) !== 0) {
+      this.examinedLength = Math.max(this.examinedLength, this.pos);
+      return '';
+    }
+    return super.next();
+  }
+
+  nextCharCode() {
+    if (this._indentationAt(this.pos) !== 0) {
+      this.examinedLength = Math.max(this.examinedLength, this.pos);
+      return INVALID_CODE_POINT;
+    }
+    return super.nextCharCode();
+  }
+
+  nextCodePoint() {
+    if (this._indentationAt(this.pos) !== 0) {
+      this.examinedLength = Math.max(this.examinedLength, this.pos);
+      return INVALID_CODE_POINT;
+    }
+    return super.nextCodePoint();
+  }
+}
 
 class Indentation extends PExpr {
   constructor(isIndent = true) {
@@ -86,6 +130,7 @@ export const IndentationSensitive = new Builder()
 Object.assign(IndentationSensitive, {
   _matchStateInitializer(state) {
     state.userData = findIndentation(state.input);
+    state.inputStream = new InputStreamWithIndentation(state);
   },
   supportsIncrementalParsing: false,
 });
--- packages/ohm-js/test/test-indentation-sensitive.js
+++ packages/ohm-js/test/test-indentation-sensitive.js
@@ -2,6 +2,38 @@ import {grammar, IndentationSensitive} from '../index.mjs';
 
 import test from 'ava';
 
+const grammarWithRule = rule =>
+  grammar(`G <: IndentationSensitive { ${rule} }`, {IndentationSensitive});
+
+test('all dedents must be consumed', t => {
+  const g = grammarWithRule('X = indent "x"');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 4: expected end of input');
+
+  const g2 = grammarWithRule('X = indent "x" dedent');
+  t.is(g2.match('  x').succeeded(), true);
+});
+
+test("can't skip over an indent", t => {
+  let g = grammarWithRule('X = "x"');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected "x"');
+
+  g = grammarWithRule('X = "x"');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected "x"');
+
+  g = grammarWithRule('X = "a".."z"');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected "a".."z"');
+
+  g = grammarWithRule('X = caseInsensitive<"x">');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected "x" (case-insensitive)');
+
+  g = grammarWithRule('X = lower');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected a lowercase letter');
+
+  // TODO: This one is unclear, `any` should probably be able to consume an indent?
+  g = grammarWithRule('X = any');
+  t.is(g.match('  x').shortMessage, 'Line 1, col 3: expected any character');
+});
+
 test('incremental parsing fails', t => {
   const matcher = grammar('G <: IndentationSensitive { start = "!!!" }', {
     IndentationSensitive,
