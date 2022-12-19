41a18b8c6150ef3b4a714a76903c1ae30cb58c01
Patrick Dubroy <pdubroy@gmail.com>
2022-12-19 16:05:29 +0100

    Make sure the typing tests are actually running!

--- packages/packaging-tests/test/test-typings.ts
+++ packages/packaging-tests/test/test-typings.ts
@@ -59,7 +59,7 @@ test('ActionDict keys - #395', () => {
 
 test('extras - getLineAndColumn & getLineAndColumnMessage', () => {
   const str = 'one\ntwo';
-  const offset = 4;
+  const offset = 0;
   const ranges = [
     [0, 2],
     [3, 6]
@@ -70,52 +70,54 @@ test('extras - getLineAndColumn & getLineAndColumnMessage', () => {
   );
 });
 
-test('getLineAndColumn - #410', t => {
+test('getLineAndColumn - #410', () => {
   const matchResult = g.match('Sup friend!');
-  t.true(matchResult.failed());
+  assert.is(matchResult.failed(), true);
   const lineAndCol = matchResult.getInterval().getLineAndColumn();
-  t.is(lineAndCol.offset, 0);
-  t.is(lineAndCol.lineNum, 1);
-  t.is(lineAndCol.colNum, 1);
-  t.is(lineAndCol.line, 'Sup friend!');
-  t.is(lineAndCol.prevLine, null);
-  t.is(lineAndCol.nextLine, null);
+  assert.is(lineAndCol.offset, 0);
+  assert.is(lineAndCol.lineNum, 1);
+  assert.is(lineAndCol.colNum, 1);
+  assert.is(lineAndCol.line, 'Sup friend!');
+  assert.is(lineAndCol.prevLine, null);
+  assert.is(lineAndCol.nextLine, null);
 });
 
-test('asIteration - #407', t => {
+test('asIteration - #407', () => {
   const g = ohm.grammar(`
     G {
       start = letters
-      letters = listOf<letter>
+      letters = listOf<letter, "">
+    }
   `);
   const s = g.createSemantics().addOperation('x', {
     start(letters) {
       return letters.asIteration().isIteration()
     }
   });
-  t.is(s(g.match('abc')).x(), true);
+  assert.is(s(g.match('abc')).x(), true);
 });
 
-
-test('Interval typings', t => {
+test('Interval typings', () => {
   const inputStr = ' Sup friend!   ';
   const matchFailure = g.match(inputStr);
-  t.true(matchFailure.failed());
+  assert.is(matchFailure.failed(), true);
 
   const interval = matchFailure.getInterval();
   interval.startIdx = 1;
   interval.endIdx = inputStr.length;
-  t.is(interval.sourceString, ' Sup friend!   ');
-  t.is(interval.contents, 'Sup friend!   ');
-  t.is(interval.trimmed().contents, 'Sup friend!');
+  assert.is(interval.sourceString, ' Sup friend!   ');
+  assert.is(interval.contents, 'Sup friend!   ');
+  assert.is(interval.trimmed().contents, 'Sup friend!');
 
   const left = interval.collapsedLeft();
   const right = interval.collapsedRight();
-  t.is(left.startIdx, interval.startIdx);
-  t.is(right.startIdx, interval.endIdx);
+  assert.is(left.startIdx, interval.startIdx);
+  assert.is(right.startIdx, interval.endIdx);
 
   const fat = interval.minus(interval.trimmed());
-  t.is(fat.length, 1);
-  t.is(fat[0].contents, '   ');
-  t.is(fat[0].relativeTo(interval).startIdx, 11);
+  assert.is(fat.length, 1);
+  assert.is(fat[0].contents, '   ');
+  assert.is(fat[0].relativeTo(interval).startIdx, 11);
 });
+
+test.run();
