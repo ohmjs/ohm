f07f7360e2985331285fe0e4c8eaeb377acfe068
Patrick Dubroy <pdubroy@gmail.com>
2025-08-01 20:29:00 +0200

    feat(ohm-js): Add support for all Unicode categories to unicodeChar
    Fixes #525

--- packages/ohm-js/src/UnicodeCategories.js
+++ packages/ohm-js/src/UnicodeCategories.js
@@ -1,30 +1,45 @@
 // These are just categories that are used in ES5/ES2015.
-// The full list of Unicode categories is here: http://www.fileformat.info/info/unicode/category/index.htm.
-export const UnicodeCategories = {
-  // Letters
-  Lu: /\p{Lu}/u,
-  Ll: /\p{Ll}/u,
-  Lt: /\p{Lt}/u,
-  Lm: /\p{Lm}/u,
-  Lo: /\p{Lo}/u,
+// The full list can extracted from https://www.unicode.org/Public/UCD/latest/ucd/extracted/DerivedGeneralCategory.txt.
 
-  // Numbers
-  Nl: /\p{Nl}/u,
-  Nd: /\p{Nd}/u,
-
-  // Marks
-  Mn: /\p{Mn}/u,
-  Mc: /\p{Mc}/u,
-
-  // Punctuation, Connector
-  Pc: /\p{Pc}/u,
-
-  // Separator, Space
-  Zs: /\p{Zs}/u,
-
-  // These two are not real Unicode categories, but our useful for Ohm.
-  // L is a combination of all the letter categories.
-  // Ltmo is a combination of Lt, Lm, and Lo.
-  L: /\p{Letter}/u,
-  Ltmo: /\p{Lt}|\p{Lm}|\p{Lo}/u,
-};
+/*
+  grep -v '^#' DerivedGeneralCategory.txt \
+    | cut -d';' -f2 \
+    | awk 'NF{print $1}' \
+    | sort -u \
+    | awk '{printf "\x27%s\x27,\n",$1}'
+ */
+export const UnicodeCategories = Object.fromEntries(
+  [
+    'Cc',
+    'Cf',
+    'Cn',
+    'Co',
+    'Cs',
+    'Ll',
+    'Lm',
+    'Lo',
+    'Lt',
+    'Lu',
+    'Mc',
+    'Me',
+    'Mn',
+    'Nd',
+    'Nl',
+    'No',
+    'Pc',
+    'Pd',
+    'Pe',
+    'Pf',
+    'Pi',
+    'Po',
+    'Ps',
+    'Sc',
+    'Sk',
+    'Sm',
+    'So',
+    'Zl',
+    'Zp',
+    'Zs'
+  ].map(alias => [alias, new RegExp(String.raw`\p{${alias}}`, 'u')])
+);
+UnicodeCategories['Ltmo'] = /\p{Lt}|\p{Lm}|\p{Lo}/u;
