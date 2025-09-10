96724f03d5a7715c6a2a72f2d57f03ac011c9092
Patrick Dubroy <pdubroy@gmail.com>
2025-09-10 10:58:33 +0200

    packaging-tests: switch from rollup to vite

---
+++ packages/packaging-tests/vite.config.mjs
@@ -0,0 +1,14 @@
+import {defineConfig} from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: 'src/index.vite.js',
+      output: {
+        entryFileNames: 'main-vite.js',
+        format: 'iife'
+      }
+    },
+    outDir: 'dist'
+  }
+});
