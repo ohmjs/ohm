9b82652d61669296919ca91bb723ddbee8794227
Patrick Dubroy <pdubroy@gmail.com>
2025-09-10 12:10:55 +0200

    typescript: use tsx to run the tests

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -2,8 +2,9 @@
 
 NODE_MAJOR_VERSION=$(node -v | cut -d'.' -f1 | sed 's/v//')
 
-# Some projects require Node >= 24; skip them altogether.
-if [ "$NODE_MAJOR_VERSION" -lt 24 ]; then
+# Some projects require type stripping (Node >= 22); skip them altogether.
+# TODO: Remove this in May 2026 when Node 20 goes EOL.
+if [ "$NODE_MAJOR_VERSION" -lt 22 ]; then
   FILTER="--filter=!@ohm-js/wasm --filter=!labrat"
 else
   FILTER=""
