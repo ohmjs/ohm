2fef8b531a1df6c8d285f8b392f3ba9ea3f97a66
Patrick Dubroy <pdubroy@gmail.com>
2026-03-08 20:11:42 +0100

    chore: end support for Node 20 (#587)
    Node 20 is EOL in a little over a month, and as we switch to using more
    and more TypeScript, it's becoming a pain to support. This PR removes
    Node 20 support and adds Node 25 to the test matrix.

--- bin/ci-test.sh
+++ bin/ci-test.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-NODE_MAJOR_VERSION=$(node -v | cut -d'.' -f1 | sed 's/v//')
-
-# Some projects require type stripping (Node >= 22); skip them altogether.
-# TODO: Remove this in May 2026 when Node 20 goes EOL.
-if [ "$NODE_MAJOR_VERSION" -lt 22 ]; then
-  FILTER="--filter=!@ohm-js/compiler --filter=!@ohm-js/semantics --filter=!@ohm-js/lang-python --filter=!@ohm-js/lang-json --filter=!labrat"
-else
-  FILTER=""
-fi
-
-pnpm $FILTER -r build && pnpm $FILTER -r --if-present api-report && pnpm lint && pnpm $FILTER -r test
+pnpm -r build && pnpm -r --if-present api-report && pnpm lint && pnpm -r test
